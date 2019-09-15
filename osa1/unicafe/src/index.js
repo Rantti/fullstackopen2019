c162a4201d784b798cc8506f02a849f8d8685595
Rantti <antti.o.eloranta@gmail.com>
2019-09-16 00:00:15 +0300

    Palautus 1.9

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,53 +1,73 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
-const Heading = ({title}) => {
+const Heading = ({ title }) => {
+  return <h1>{title}</h1>;
+};
+
+const Statistic = ({ text, value }) => {
+  return (
+    <p>
+      {text}: {value}
+    </p>
+  );
+};
+
+const Statistics = ({ good, bad, neutral }) => {
+  if (good === 0 && bad === 0 && neutral === 0) {
     return (
-        <h1>{title}</h1>
-    )
-}
-
-const Statistic = ({text, value}) => {
-    return (
-        <p>{text}: {value}</p>
-    )
-}
-
-const App = () => {
-  // tallenna napit omaan tilaansa
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const onGoodClick = () => {
-    setGood(good + 1);
-  }
-
-  const onNeutralClick = () => {
-    setNeutral(neutral + 1);
-  }
-
-  const onBadClick = () => {
-    setBad(bad + 1);
+      <div>
+        <p>No feedback given</p>
+      </div>
+    );
   }
 
   return (
     <div>
-        <Heading title="give feedback" />
-        <button onClick={onGoodClick}>good</button>
-        <button onClick={onNeutralClick}>neutral</button>
-        <button onClick={onBadClick}>bad</button>
-        <Heading title="statistics" />
-        <Statistic text="good" value={good} />
-        <Statistic text="neutral" value={neutral} />
-        <Statistic text="bad" value={bad} />
-        <Statistic text="all" value={good + neutral + bad} />
-        <Statistic text="average" value={(good - bad) / (good + bad + neutral) || 0} />
-        <Statistic text="positive" value={`${good / (good + bad + neutral) * 100 || 0} %`} />
+      <Statistic text='good' value={good} />
+      <Statistic text='neutral' value={neutral} />
+      <Statistic text='bad' value={bad} />
+      <Statistic text='all' value={good + neutral + bad} />
+      <Statistic
+        text='average'
+        value={(good - bad) / (good + bad + neutral) || 0}
+      />
+      <Statistic
+        text='positive'
+        value={`${(good / (good + bad + neutral)) * 100 || 0} %`}
+      />
     </div>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />,
-  document.getElementById('root')
-)
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onGoodClick = () => {
+    setGood(good + 1);
+  };
+
+  const onNeutralClick = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const onBadClick = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <Heading title='give feedback' />
+      <button onClick={onGoodClick}>good</button>
+      <button onClick={onNeutralClick}>neutral</button>
+      <button onClick={onBadClick}>bad</button>
+      <Heading title='statistics' />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
